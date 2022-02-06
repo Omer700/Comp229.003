ba5d50d7899c175a72fa39ce668b774d1b532d37
Omer700 <98240955+Omer700@users.noreply.github.com>
2022-02-05 21:47:58 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,50 @@
+/*
+ Name: Omer Suleman
+ Student No: 301174085
+ */
+
+//installed packages
+let createError = require("http-errors");
+let express = require("express");
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let logger = require("morgan");
+
+let indexRouter = require("./routes/index");
+let usersRouter = require("./routes/users");
+
+let app = express();
+
+// view setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "node_modules")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.post('/contact',(req,res,next)=>{
+  res.redirect('/home');
+});
+// catch error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
